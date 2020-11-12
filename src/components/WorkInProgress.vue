c8db762d34a4151c58da8759b02308551d4a5bc1
Username4723 <11415806+Username4723@users.noreply.github.com>
2020-11-11 22:05:23 -0600

    Add WIP message

---
+++ src/components/WorkInProgress.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-card-title>501: Not Implemented</b-card-title>
+    The problem sits between the keyboard and the chair. Specifically, <i>my</i> keyboard and char.
+    <br/>
+    In other words, check back later and I might have gotten around to writing this part of the site.
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,14 @@ Vue.use(VueRouter)
 import Server from "../components/projects/server/Server.vue"
 import Dnd from "../components/projects/Dnd.vue"
 import NotFound from "../components/NotFound.vue"
+import WorkInProgress from "../components/WorkInProgress.vue"
 
 const routes = [
+  { path: '/', component: WorkInProgress },
+  { path: '/about/skills', component: WorkInProgress },
+  { path: '/about/competitions', component: WorkInProgress },
+  { path: '/about/experience', component: WorkInProgress },
+  { path: '/about/contact', component: WorkInProgress },
   { path: '/projects/server', component: Server },
   { path: '/projects/dnd', component: Dnd },
   { path: '*', component: NotFound }
