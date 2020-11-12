e5800aec01177731636a4fc5755977a7e5d29266
Username4723 <11415806+Username4723@users.noreply.github.com>
2020-11-11 21:17:02 -0600

    Add Navbar

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,6 +2,24 @@
   <div style="padding: 3%">
     <b-card bg-variant="transparent" text-variant="light" border-variant="dark">
       <b-card-title>Nathan Nguyen</b-card-title>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand to="/">Home</b-navbar-brand>
+        <b-collapse id="nav-text-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item-dropdown text="About Me">
+              <b-dropdown-item to="/about/skills">Skills & Talents</b-dropdown-item>
+              <b-dropdown-item to="/about/competitions">Past Competitions</b-dropdown-item>
+              <b-dropdown-item to="/about/experience">Experience & Education</b-dropdown-item>
+              <b-dropdown-item to="/about/contact">Contact Me</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown text="Projects">
+              <b-dropdown-item to="/projects/dnd">Dungeons & Dragons</b-dropdown-item>
+              <b-dropdown-item to="/projects/servers">Minecraft Server</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </b-card>
     <br/>
     <b-card bg-variant="transparent" text-variant="light" border-variant="dark">
